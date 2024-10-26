4b6eefbcd9bb005366263f1956e94c9a56e0d2bb
Aisha Farah <aishafarah789@hotmail.com>
2024-10-26 21:58:06 +0100

    standardized naming and removed constraint names
    Changed all primary keys to ID, removed constraint names for simplicity and changed the rating range.

--- server/database/migrations/000004_create_product_images_table.up.sql
+++ server/database/migrations/000004_create_product_images_table.up.sql
@@ -1,12 +1,11 @@
 CREATE TABLE product_images (
-    image_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     product_id BIGINT,
     image VARCHAR(255) NOT NULL,
     primary_image boolean NOT NULL DEFAULT FALSE, 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
-    CONSTRAINT fk_product
-      FOREIGN KEY (product_id) REFERENCES products(product_id)
+      FOREIGN KEY (product_id) REFERENCES products(id)
 
 );
--- server/database/migrations/000005_create_product_reviews_table.up.sql
+++ server/database/migrations/000005_create_product_reviews_table.up.sql
@@ -1,16 +1,13 @@
 CREATE TABLE product_reviews(
-    review_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     product_id BIGINT,
     user_id BIGINT,
-    rating SMALLINT NOT NULL CHECK(rating BETWEEN 0 AND 5),
+    rating SMALLINT NOT NULL CHECK(rating BETWEEN 1 AND 10),
     comment TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
-    CONSTRAINT fk_product
-      FOREIGN KEY(product_id) REFERENCES products(product_id),
-
-    CONSTRAINT fk_user
+      FOREIGN KEY(product_id) REFERENCES products(id),
       FOREIGN KEY(user_id) REFERENCES users(id)
     
 );
