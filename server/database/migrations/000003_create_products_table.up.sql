95623fd18305a063cd8baf402da550224a8675f6
Aisha Farah <aishafarah789@hotmail.com>
2024-10-26 16:46:30 +0100

    Created products and product_categories table

---
+++ server/database/migrations/000003_create_products_table.up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE products (
+    product_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    stock INT NOT NULL,
+    category_id BIGINT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_category
+        FOREIGN KEY (category_id) REFERENCES product_categories(category_id)
+        
+);
