277bbeebfcc93e8a86c3df9f68d6c5d99466e596
ruwaida11 <107909873+ruwaida11@users.noreply.github.com>
2024-10-27 14:15:30 +0000

    ordered items table down and up

---
+++ server/database/migrations/000006_create_ordered_items_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE ordered_items(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    order_id BIGINT,
+    product_id BIGINT,
+    quantity INT NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    FOREIGN KEY(product_id) REFERENCES products(id),
+    FOREIGN KEY(order_id) REFERENCES orders(id)    
+);
