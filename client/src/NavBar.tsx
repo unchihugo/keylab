e58a492a796316701bb9ab9942f9efe6e267d36f
Hugo <hu8go8li@gmail.com>
2024-11-21 20:07:23 +0000

    Implement sign-in UI anf functionality, improve AuthContext, add temp logout button in NavBar

--- client/src/NavBar.tsx
+++ client/src/NavBar.tsx
@@ -6,7 +6,7 @@ import { ShoppingCart, UserRound, Heart } from 'lucide-react';
 import { useAuth } from './AuthContext';
 
 const NavBar: React.FC = () => {
-    const { isAuthenticated } = useAuth(); // gets auth state from AuthContext
+    const { isAuthenticated, logout } = useAuth(); // gets auth state from AuthContext
 
     return (
         // TODO: add a mobile menu
@@ -25,6 +25,8 @@ const NavBar: React.FC = () => {
                 </div>
                 <div className='w-1/3 flex justify-end items-center'>
                     {isAuthenticated ? (<>
+                        {/* TODO: remove temporary logout button */}
+                        <button onClick={() => logout()} className='bg-secondary-dark text-white px-6 py-2 text-sm rounded-full'>temp logout btn</button>
                         <LinkButton to="/profile" buttonClassNames='ms-2 h-10 w-10 bg-white' Icon={UserRound} />
                         <LinkButton to="/favorites" buttonClassNames='ms-2 h-10 w-10 bg-white' Icon={Heart} />
                     </>
