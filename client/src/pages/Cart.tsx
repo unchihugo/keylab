ed7ab1a86749cd6d000770b47e87dc7590baf508
Finlay Smith <161974589+fxnners@users.noreply.github.com>
2024-11-30 15:02:13 +0000

    added requested changes

--- client/src/pages/Cart.tsx
+++ client/src/pages/Cart.tsx
@@ -1,8 +1,7 @@
-/** @format */
-
-import React, { useState } from "react"
+import { useState } from "react"
 import Divider from "../components/Divider"
 import InputBox from "../components/InputBox"
+import LinkButton from "../components/LinkButton"
 
 export default function Cart() {
 	{
@@ -82,19 +81,19 @@ export default function Cart() {
 	}
 	return (
 		<div className="flex bg-primary justify-center items-start">
-			<div className="flex max-w-screen-lg w-full">
-				<div className="w-1/2 bg-white mt-40 drop-shadow-cartoon rounded-lg border border-black h-fit">
+			<div className="flex flex-col w-full lg:flex-row max-w-screen-lg">
+				<div className="w-full lg:w-1/2 bg-white mt-40 drop-shadow-cartoon rounded-lg border border-black h-fit">
 					<div className="text-3xl font-display pl-2 mt-6">
 						Basket Summary
 					</div>
 					<Divider />
 					<div className="p-2 font-display">
-						Your Items:
+						Your Items:   
 						<Divider />
-						<div className="Items space-y-4">
+						<div className="Items space-y-4 font-body">
 							{itemsInBasket.map((item) => (
 								<div>
-									<ul
+									<ul 
 										key={item.product}
 										className="flex justify-between items-center border-black pb-2">
 										<li>{item.name}</li>
@@ -102,6 +101,9 @@ export default function Cart() {
 										<li className="text-gray-400">
 											{item.quantity}
 										</li>
+                                        <li className="text-gray-400"> 
+                                        £{item.price * item.quantity}
+                                        </li>
 									</ul>
 									<button
 										onClick={() => handleDelete(item)}
@@ -120,24 +122,25 @@ export default function Cart() {
 				</div>
 
 				{/* order section of the cart page */}
-				<div className="flex flex-col pr-10 items-end justify-center h-screen w-1/2">
+				<div className="w-full lg:w-1/2 flex flex-col pr-10 items-end justify-center h-screen w-1/2">
 					<div className="px-8 py-8 pb-40 mt-20 bg-white drop-shadow-cartoon rounded-lg border border-black">
 						<div className="text-4xl font-display">
 							Order Summary
 						</div>
 						<Divider />
-						<div className="p-4 mt-9 font-display border-t border-b border-black">
+						<div className="p-4 mt-9 font-body border-t border-b border-black">
 							<text>Subtotal: </text>£{totalPrice}
 						</div>
-						<div className="text-2xl font-display mt-3 ml-9">
+						<div className="text-2xl font-body mt-3 ml-9">
 							Do you have a code?
 						</div>
 						{/* @ts-expect-error Async*/}
 						<InputBox />
-						<div className="flex justify-center">
-							<button className="px-10 py-5 bg-sky-400 text-white rounded-lg absolute bottom-5">
-								Checkout
-							</button>
+						<div className="flex">
+							<LinkButton to="/cart/checkout" 
+              buttonClassNames="px-10 py-5 bg-secondary text-white rounded-lg absolute bottom-12 ml-24" 
+              text="Checkout">
+							</LinkButton>
 						</div>
 					</div>
 				</div>
