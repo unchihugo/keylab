abf00bc6ce97058c140a5543acd495b3139111ff
ruwaida11 <107909873+ruwaida11@users.noreply.github.com>
2024-12-02 12:19:32 +0000

    checkout page
    simple design doesnt have functionality stuff yet

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -12,9 +12,10 @@ import NotFound from "./pages/NotFound"
 import SignIn from "./pages/auth/sign-in"
 import Register from "./pages/auth/register"
 import Product from "./pages/products/[slug]"
-import Cart from './pages/Cart'
+import Cart from "./pages/Cart"
 import ProtectedRoute from "./components/ProtectedRoute"
 import About from "./pages/About"
+import Checkout from "./pages/Checkout"
 
 const router = createBrowserRouter([
 	{
@@ -23,6 +24,7 @@ const router = createBrowserRouter([
 		children: [
 			{ path: "/", element: <Home /> },
 			{ path: "/cart", element: <Cart /> },
+			{ path: "/checkout", element: <Checkout /> },
 			{ path: "/example", element: <div>Example</div> },
 			{
 				path: "/protected",
