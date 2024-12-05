e43f20ea81061246001f93c210e599d7ac4df95b
ruwaida11 <107909873+ruwaida11@users.noreply.github.com>
2024-12-05 16:53:27 +0000

    changed about us page
    adjusted colour, layout and text

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import Divider from "../components/Divider";
+import Divider from "../components/Divider"
 
 export default function About() {
 	return (
@@ -8,23 +8,32 @@ export default function About() {
 			<div className="flex flex-col w-full max-w-3xl px-4 space-y-6">
 				{/* About Us */}
 				<div className="px-6 py-8 bg-white drop-shadow-cartoon rounded-lg border border-black">
-					<div className="text-2xl font-display text-black mb-4">About Us</div>
+					<div className="text-2xl font-display text-black mb-4">
+						About Us
+					</div>
 					<Divider />
 					<div className="flex flex-col space-y-6 font-body text-gray-600">
 						{/* Vision */}
 						<section>
-							<h2 className="text-xl font-semibold text-gray-800 mb-4">Our Vision</h2>
+							<h2 className="text-xl font-semibold text-gray-800 mb-4">
+								Our Vision
+							</h2>
 							<p>
-								At Keylab, we specialise in creating a unique experience for our customers by offering a range 
-                                of high-quality products designed to enhance both function and aesthetics. Whether it's smooth 
-                                switches, keycaps or the chance to completely personalise your own keyboard,
-                                 we have it all!
+								At Keylab, we specialise in creating a unique
+								experience for our customers by offering a range
+								of high-quality products designed to enhance
+								both function and aesthetics. Whether it's
+								smooth switches, keycaps or the chance to
+								completely personalise your own keyboard, we
+								have it all!
 							</p>
 							<p className="mt-4">
-								Our vision is to empower creators, professionals, and gamers with tools that
-								enhance performance, comfort and creativity, allowing people to express themselves. 
-								Our commitment to innovation drives us to give everyone the chance to elevate their 
-                                experince.
+								Our vision is to empower creators,
+								professionals, and gamers with tools that
+								enhance performance, comfort and creativity,
+								allowing people to express themselves. Our
+								commitment to innovation drives us to give
+								everyone the chance to elevate their experince.
 							</p>
 						</section>
 					</div>
@@ -38,5 +47,5 @@ export default function About() {
 				</div>
 			</div>
 		</div>
-	);
+	)
 }
