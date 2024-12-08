052187c56c646c49211f96ec527bd5c3314fd4dd
Michael T. <66504185+michaeltukdev@users.noreply.github.com>
2024-12-08 22:17:48 +0000

    Product fixes

--- client/src/types/ProductImage.ts
+++ client/src/types/ProductImage.ts
@@ -1,14 +1,11 @@
 /** @format */
 
 export interface ProductImage {
-	data: {
-		id: number
-		product_id: number
-		image: string
-		url?: string
-		primary_image: boolean
-		created_at?: string | null
-		updated_at?: string | null
-	}
-	message: string
+	id: number
+	product_id: number
+	image: string
+	url?: string
+	primary_image: boolean
+	created_at?: string | null
+	updated_at?: string | null
 }
