9c47cf36b3f3e459eef5fe2dc89c132c353b0c6c
Hugo <hu8go8li@gmail.com>
2025-02-19 21:53:19 +0000

    format ProductReview

--- client/src/types/ProductReview.ts
+++ client/src/types/ProductReview.ts
@@ -1,11 +1,11 @@
 /** @format */
 
 export interface ProductReview {
-    id: number
-    product_id: number
-    user_id: number
-    rating: number
-    comment: string
-    created_at?: string | null
-    updated_at?: string | null
+	id: number
+	product_id: number
+	user_id: number
+	rating: number
+	comment: string
+	created_at?: string | null
+	updated_at?: string | null
 }
