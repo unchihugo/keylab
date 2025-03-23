22e408e6ee5230693e006ffa4b84c87897ab3715
Aisha Farah <aishafarah789@hotmail.com>
2025-03-23 14:10:49 +0000

    fixed route

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -88,7 +88,7 @@ func RegisterRoutes(e *echo.Echo, sessionStore *sessions.CookieStore, db *gorm.D
 
 	adminUserGroup := adminGroup.Group("/users")
 	adminUserGroup.GET("", h.GetAllUsers)
-	adminUserGroup.PUT("/users/:id", h.UpdateUserByAdmin)
+	adminUserGroup.PUT("/:id", h.UpdateUserByAdmin)
 	//adminUserGroup.DELETE("/users/:id", h.DeleteUserByAdmin)
 
 	adminOrdersGroup := adminGroup.Group("/orders")
